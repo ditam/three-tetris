1608aa3c17304e89ae9e2f0c75c88c3a133756c1
ditam <ditam@users.noreply.github.com>
2017-02-19 18:41:13 +0100

    add base setup

---
+++ three-tetris.js
@@ -0,0 +1,32 @@
+'use strict';
+var scene, camera, renderer;
+var geometry, material, mesh;
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.z = 1000;
+
+  geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+  mesh = new THREE.Mesh( geometry, material );
+  mesh.position.set( 0, 0, 0 );
+  scene.add( mesh );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  document.getElementById('main-renderer-target').appendChild( renderer.domElement );
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  scene.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+}
+
+init();
+animate();
