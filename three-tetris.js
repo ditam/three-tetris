79c479af810add8acddb1d1c24dfabba0e018ecc
ditam <ditam@users.noreply.github.com>
2017-03-01 00:42:45 +0100

    add single free block and drop mechanic

--- three-tetris.js
+++ three-tetris.js
@@ -6,8 +6,14 @@ var CONSTANTS = Object.freeze({
  OUTER_RADIUS: 300,
  BLOCK_HEIGHT: 50,
  SEGMENT_COUNT: 12,
+ NEW_BLOCK_ROW: 9,
+ NEW_BLOCK_COL: 0,
  LINE_HEIGHT: 50
 });
+
+function getRandomInt(min, max) { // both ends inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 var MATERIALS = Object.freeze({
   RED: new THREE.MeshPhongMaterial({
@@ -30,9 +36,32 @@ var MATERIALS = Object.freeze({
   })
 });
 
+function getRandomMaterial(){
+  var materialKeys = Object.keys(MATERIALS);
+  var randomKey = materialKeys[getRandomInt(0,materialKeys.length-1)];
+  return MATERIALS[randomKey];
+}
+
+var freeBlockPosition = {
+  row: CONSTANTS.NEW_BLOCK_ROW,
+  col: CONSTANTS.NEW_BLOCK_COL
+};
+
+function addNewBlock(){
+  var startRow = CONSTANTS.NEW_BLOCK_ROW;
+  var startCol = CONSTANTS.NEW_BLOCK_COL;
+  var block = {
+    material: getRandomMaterial()
+  };
+
+  //TODO: assert empty
+  model.blocks[startRow][startCol] = block;
+  regenerateMeshes();
+}
+
 var model = {
   blocks: [ // A 2D matrix of CONSTANTS.SEGMENT_COUNT long rows
-    [
+    [ // row 0
       undefined,
       undefined,
       {
@@ -46,13 +75,21 @@ var model = {
         material: MATERIALS.BLUE
       }
     ],
-    [
+    [ // row 1
       undefined,
       undefined,
       {
         material: MATERIALS.GREEN
       }
-    ]
+    ],
+    [], // r2
+    [], // r3
+    [], // r4
+    [], // r5
+    [], // r6
+    [], // r7
+    [], // r8,
+    []  // r9
   ]
 };
 
@@ -65,7 +102,8 @@ document.onkeypress = function(event) {
       rotateBoard('left');
       break;
     case 'KeyS':
-      console.warn('DROP not implemented');
+      dropByOne();
+      // TODO: add collision detection and merging of freeblock+blocks
       break;
     case 'KeyD':
       rotateBoard('right');
@@ -73,6 +111,19 @@ document.onkeypress = function(event) {
   }
 };
 
+// TODO: clear up board and blocks terminology
+function dropByOne() {
+  // get block
+  var freeBlock = model.blocks[freeBlockPosition.row][freeBlockPosition.col];
+  // remove from board
+  delete model.blocks[freeBlockPosition.row][freeBlockPosition.col];
+  // drop position by one row
+  freeBlockPosition.row -= 1;
+  // add to model in new position -- TODO: this might be a good place for collision detection
+  model.blocks[freeBlockPosition.row][freeBlockPosition.col] = freeBlock;
+
+  regenerateMeshes();
+}
 
 var sliceAngle = Math.PI*2 / CONSTANTS.SEGMENT_COUNT;
 
@@ -124,23 +175,43 @@ function constructSliceGeometry(lineIndex, segmentIndex) {
   return new THREE.ConvexGeometry(points);
 }
 
-function init() {
-  scene = new THREE.Scene();
+var currentMeshes = [];
 
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 1000;
+// This is far from optimal (we could probably just move the meshes, or only regenerate those that changed), but it'll do for now.
+function regenerateMeshes() {
+  console.log('regenerating meshes. Current model:',model.blocks);
+
+  currentMeshes.forEach(function(mesh){
+    scene.remove(mesh);
+  });
+  currentMeshes = [];
 
   for(var rowIndex = 0; rowIndex < model.blocks.length; rowIndex++){
     for(var colIndex = 0; colIndex < model.blocks[rowIndex].length; colIndex++){
       var block = model.blocks[rowIndex][colIndex];
       if (block) {
+        console.log('adding mesh at ',rowIndex,colIndex);
         var geometry = constructSliceGeometry(rowIndex, colIndex);
         var mesh = new THREE.Mesh( geometry, block.material );
         mesh.position.set( 0, 0, 0 );
-        scene.add( mesh );
+        currentMeshes.push( mesh );
       }
     }
   }
+
+  currentMeshes.forEach(function(mesh){
+    scene.add(mesh);
+  });
+}
+
+function init() { // TODO: extract constants
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.z = 400;
+  camera.position.y = -1000;
+
+  regenerateMeshes();
 
   var lights = [];
   lights[ 0 ] = new THREE.PointLight( 0xffffff );
@@ -177,3 +248,4 @@ function animate() {
 
 init();
 animate();
+addNewBlock();
